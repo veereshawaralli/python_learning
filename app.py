0955e866a0b8256fc66c005f5c3834ed1ee13d92
veeresh <medicoveeresh8818@gmail.com>
2025-11-26 14:52:54 +0530

    datatype

---
+++ app.py
@@ -0,0 +1,18 @@
+# print("hello world")
+# print("ASP.NET Core with Python")
+# a=1
+# b=2
+# c=3
+# d=a
+# a=b
+# b=c
+# c=d
+# print(a,b,c)
+# name=""
+
+a=10
+b=20.5
+c="Hello"
+print(type(a))
+print(type(b))
+print(type(c))
