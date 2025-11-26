fc7dfde3f971effebdb9dd442d6a6a3c5705b4ea
veeresh <medicoveeresh8818@gmail.com>
2025-11-26 16:19:16 +0530

    conditional statements

--- app.py
+++ app.py
@@ -10,9 +10,20 @@
 # print(a,b,c)
 # name=""
 
-a=10
-b=20.5
-c="Hello"
-print(type(a))
-print(type(b))
-print(type(c))
+# a=10
+# b=20.5
+# c="Hello"
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# a=int(input("Enter a number: "))
+# if a%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+a=int(input("Enter student number: "))
+if a>=35:
+    print("Pass")
+else:
+    print("Fail")
